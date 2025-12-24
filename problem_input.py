f6f72066127e31849286d563abb834dbda6fe530
harshtelangework-web <harshtelangework@gmail.com>
2025-12-24 16:04:21 +0530

    Add files via upload

---
+++ problem_input.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+########### Write your code below ###############
+
+# Take string input and print the string input
+
+# Take integer input and add 10 to the integer input and print
+
+# Take floating-point input and multiply the float input by 10 and print
+
+########### Write your code above ###############
+
+# String Input and Print
+text = input()
+print(text)
+
+#Integer Input And Add
+n = int(input())
+print(n+10)
+
+# Float Input and Multiply
+f = float(input())
+print(f*10)
