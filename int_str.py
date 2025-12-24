5f137249032a75c8ba84e3fa4f6260d9b87181b2
harshtelangework-web <harshtelangework@gmail.com>
2025-12-24 19:05:05 +0530

    Add files via upload

---
+++ int_str.py
@@ -0,0 +1,7 @@
+#Given three inputs that are stored in the variables a, b, and c. 
+#You need to print a a times and b b times  in a single line separated by c.
+a = input()
+b = input()
+c = input()
+
+print((a*int(a)) + c + (b*int(b)))
