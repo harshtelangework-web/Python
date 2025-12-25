f24f9c345b1c1e1e4e20a8f5cce3dd83b3a8bdbf
harshtelangework-web <harshtelangework@gmail.com>
2025-12-25 17:55:03 +0530

    Add files via upload

---
+++ for-loop.py
@@ -0,0 +1,6 @@
+#Writing for loop in Python is a tad different from C++ and Java counterparts. In this question, we'll learn to print table by using the for loop.
+
+#You are given a number N, you need to print its multiplication table.
+
+for i in range(1 , 11):
+ print (5*i)
